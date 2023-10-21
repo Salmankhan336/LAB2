d1956274b136fc3507f2bbc84bb18c0154b2bc04
John Doe <khamar6660@gmail.com>
2023-10-21 04:01:22 -0700

    3rd commit

--- dollar.c
+++ dollar.c
@@ -6,5 +6,11 @@ int main()
     printf("enter the amount in us dollars");
     scanf("%if", &usdollars);
     
+    //calculate the exchange fee which is 10% of the total amount
+    exchangefee = 0.10 * usdollars;
+
+    //calculate te amount after deducting the exchange fee
+    double remainingdollars = usdollars - exchangefee;
+
 
 }
